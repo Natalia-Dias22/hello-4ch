61c3bf3de366dea59e2b1a516d698bb7154376f7
Natalia Jorge <n.jorge@4intelligence.ai>
2026-02-05 10:44:56 -0300

    Add version endpoint

--- src/main.py
+++ src/main.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def health_check():
     return jsonify({'status': 'OK'}), 200
 
+# NOVO ENDPOINT! v2
+@app.route('/version', methods=['GET'])
+def version():
+    return jsonify({
+        'version': '2.0',
+        'app': 'hello-4ch',
+        'status': 'running'
+    }), 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
